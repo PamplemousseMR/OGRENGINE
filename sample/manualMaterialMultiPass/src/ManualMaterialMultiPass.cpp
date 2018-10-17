2787f37d757fa63065befd43d9ea41a928cc9903
pamplemousseMR <romain.manciaux@gmail.com>
2018-10-17 21:06:15 +0200

    feat(sample): add manual material multi pass

---
+++ sample/manualMaterialMultiPass/src/ManualMaterialMultiPass.cpp
@@ -0,0 +1,188 @@
+#include "ManualMaterialMultiPass.hpp"
+
+#include <iostream>
+
+ManualMaterialMultiPass::ManualMaterialMultiPass() :
+    FrameListener(),
+    m_renderSystemLoader(),
+    m_resourceLoader("../../config/resources.cfg"),
+	m_log(nullptr),
+	m_root(nullptr),
+	m_sceneManager(nullptr),
+	m_renderWindow(nullptr),
+	m_viewport(nullptr),
+	m_camera(nullptr),
+    m_light(nullptr),
+    m_cube(nullptr),
+    m_cameraNode(nullptr),
+    m_material("ManualMaterialMultiPass")
+{
+	m_log = OGRE_NEW Ogre::LogManager();
+    m_log->createLog("log.txt", false, false);
+
+#ifdef _DEBUG
+    m_root = OGRE_NEW::Ogre::Root("../../config/plugins_d.cfg");
+#else
+    m_root = OGRE_NEW ::Ogre::Root("../../config/plugins.cfg");
+#endif
+}
+
+ManualMaterialMultiPass::~ManualMaterialMultiPass()
+{
+    if(m_root != nullptr) OGRE_DELETE m_root;
+	if(m_log != nullptr) OGRE_DELETE m_log;
+}
+
+void ManualMaterialMultiPass::start()
+{
+	m_root->addFrameListener(this);
+
+    m_renderSystemLoader.load(::RenderSystemManager::OPENGL3);
+    m_renderSystemLoader.setColourDepth(::RenderSystemManager::DEPTH_32);
+    m_renderSystemLoader.setDisplayFrequency(::RenderSystemManager::FREQUENCY_60);
+    m_renderSystemLoader.setFSAA(::RenderSystemManager::FSAA_8);
+    m_renderSystemLoader.setFullScreen(false);
+    m_renderSystemLoader.setRTTPreferredMode(::RenderSystemManager::RTT_FBO);
+    m_renderSystemLoader.setVSync(false);
+    m_renderSystemLoader.setVSyncInterval(::RenderSystemManager::INTERVAL_1);
+    m_renderSystemLoader.setVideoMode(::RenderSystemManager::MODE_800x600);
+
+	loadScene();
+
+    m_resourceLoader.load();
+    m_resourceLoader.initialise("ManualMaterialMultiPass");
+
+    m_material.load();
+
+	createObject();
+}
+
+void ManualMaterialMultiPass::stop()
+{
+	destroyObject();
+
+    m_resourceLoader.unload();
+
+    unloadScene();
+
+	m_root->removeFrameListener(this);
+}
+
+void ManualMaterialMultiPass::run()
+{
+    int i=0;
+    while (++i < 10000)
+    {
+        if(m_renderWindow->isClosed())
+        {
+            return;
+        }
+        if(!m_root->renderOneFrame())
+        {
+            return;
+        }
+    }
+}
+
+void ManualMaterialMultiPass::loadScene()
+{
+	m_renderWindow = m_root->initialise(true, "OGRENGINE_RenderWindow");
+
+	m_sceneManager = m_root->createSceneManager();
+    m_sceneManager->setAmbientLight(::Ogre::ColourValue(1.0, 1.0, 1.0, 1.0));
+
+	m_camera = m_sceneManager->createCamera("OGRENGINE_Camera");
+    m_camera->setPosition(Ogre::Vector3(0, 0, 500));
+	m_camera->setNearClipDistance(Ogre::Real(0.01));
+    m_camera->setFarClipDistance(Ogre::Real(1000));
+	m_camera->setAutoAspectRatio(true);
+
+	m_viewport = m_renderWindow->addViewport(m_camera);
+	m_viewport->setBackgroundColour(Ogre::ColourValue(0.75, 0.75, 0.75));
+
+	m_camera->setAspectRatio(Ogre::Real(m_viewport->getHeight() / m_viewport->getWidth()));
+
+	m_cameraNode = m_sceneManager->getRootSceneNode()->createChildSceneNode();
+    m_cameraNode->attachObject(m_camera);
+}
+
+void ManualMaterialMultiPass::unloadScene()
+{
+    m_cameraNode->detachObject(m_camera);
+    m_sceneManager->getRootSceneNode()->removeChild(m_cameraNode);
+
+    m_renderWindow->removeViewport(0);
+
+	m_sceneManager->destroyCamera(m_camera);
+
+    m_root->destroySceneManager(m_sceneManager);
+
+    m_renderWindow->destroy();
+	m_root->destroyRenderTarget(m_renderWindow);
+}
+
+void ManualMaterialMultiPass::createObject()
+{
+    // Light
+	//----------------------------------
+	{
+        m_light = m_sceneManager->createLight("OGRENGINE_Light");
+        m_light->setType(Ogre::Light::LT_POINT);
+        m_light->setPosition(Ogre::Vector3(500, 0, 1000));
+        m_light->setDiffuseColour(Ogre::ColourValue(1, 1, 1));
+        m_light->setSpecularColour(Ogre::ColourValue(1, 1, 1));
+        m_sceneManager->getRootSceneNode()->attachObject(m_light);
+	}
+    // Cube
+	//----------------------------------
+	{
+        ::Ogre::Entity* cube = m_sceneManager->createEntity("OGRENGINE_Cube", "cube.mesh", "ManualMaterialMultiPass");
+        cube->setMaterialName("ManualMaterialMultiPass");
+		cube->setCastShadows(false);
+
+        m_cube = m_sceneManager->getRootSceneNode()->createChildSceneNode();
+        m_cube->attachObject(cube);
+	}
+}
+
+void ManualMaterialMultiPass::destroyObject()
+{
+    // Cube
+    //----------------------------------
+    {
+        ::Ogre::MovableObject* mov = m_cube->detachObject("OGRENGINE_Cube");
+        m_sceneManager->getRootSceneNode()->removeChild(m_cube);
+        m_sceneManager->destroyMovableObject(mov);
+    }
+    // Light
+	//----------------------------------
+	{
+        ::Ogre::MovableObject* mov = m_sceneManager->getRootSceneNode()->detachObject("OGRENGINE_Light");
+		m_sceneManager->destroyMovableObject(mov);
+	}
+}
+
+
+// ================================= Events =================================
+
+bool ManualMaterialMultiPass::frameStarted(const Ogre::FrameEvent&)
+{
+	return true;
+}
+
+bool ManualMaterialMultiPass::frameEnded(const Ogre::FrameEvent&)
+{
+	return true;
+}
+
+bool ManualMaterialMultiPass::frameRenderingQueued(const Ogre::FrameEvent&)
+{
+    // Rotate the cube
+	//---------------------------------
+	{
+        m_cube->yaw(Ogre::Degree(Ogre::Real(0.02)));
+        m_cube->pitch(Ogre::Degree(Ogre::Real(0.03)));
+        m_cube->roll(Ogre::Degree(Ogre::Real(0.01)));
+	}
+	return true;
+}
---
+++ sample/manualMaterialMultiPass/src/Material.cpp
@@ -0,0 +1,222 @@
+#include "Material.hpp"
+
+#include <OGRE/OgreMaterialManager.h>
+#include <OGRE/OgreTechnique.h>
+#include <OGRE/OgrePass.h>
+#include <OGRE/OgreHighLevelGpuProgram.h>
+#include <OGRE/OgreHighLevelGpuProgramManager.h>
+#include <OGRE/OgreTextureManager.h>
+#include <OGRE/OgrePass.h>
+
+#include <iostream>
+
+Material::Material(const std::string& _name, const std::string& _group) :
+    m_name(_name),
+    m_group(_group)
+{
+}
+
+Material::~Material()
+{
+}
+
+void Material::load()
+{
+    std::cout << "[" << __FUNCTION__ << "] " << m_name << "..." << std::endl;
+
+    m_material = ::Ogre::MaterialManager::getSingleton().create(m_name, m_group);
+    ::Ogre::Technique* technique = m_material->getTechnique(0);
+    ::Ogre::Pass* pass0 = technique->getPass(0);
+    ::Ogre::Pass* pass1 = technique->createPass();
+    ::Ogre::Pass* pass2 = technique->createPass();
+
+    pass0->setAmbient(::Ogre::ColourValue(0.2f, 0.0f, 0.0f, 1.0));
+    pass0->setDiffuse(::Ogre::ColourValue(1.0f, 0.0f, 0.0f, 1.0));
+    pass0->setSpecular(::Ogre::ColourValue(1.0f, 0.0f, 0.0f, 1.0));
+    pass0->setShininess(33);
+
+    pass1->setAmbient(::Ogre::ColourValue(0.0f, 0.2f, 0.0f, 1.0));
+    pass1->setDiffuse(::Ogre::ColourValue(0.0f, 1.0f, 0.0f, 1.0));
+    pass1->setSpecular(::Ogre::ColourValue(0.0f, 1.0f, 0.0f, 1.0));
+    pass1->setShininess(33);
+    pass1->setSceneBlending(::Ogre::SceneBlendType::SBT_ADD);
+
+    pass2->setAmbient(::Ogre::ColourValue(0.0f, 0.0f, 0.2f, 1.0));
+    pass2->setDiffuse(::Ogre::ColourValue(0.0f, 0.0f, 1.0f, 1.0));
+    pass2->setSpecular(::Ogre::ColourValue(0.0f, 0.0f, 1.0f, 1.0));
+    pass2->setShininess(33);
+    pass2->setSceneBlending(::Ogre::SceneBlendType::SBT_ADD);
+
+    {
+        Ogre::HighLevelGpuProgramPtr vertex = Ogre::HighLevelGpuProgramManager::getSingletonPtr()->createProgram(m_name + "_VS", m_group, "glsl", Ogre::GpuProgramType::GPT_VERTEX_PROGRAM);
+        vertex->setSource("#version 440 core\n"
+                          "\n"
+                          "layout(location = 0) in vec3 position;\n"
+                          "layout(location = 2) in vec3 normal;\n"
+                          "layout(location = 8) in vec2 uv0;\n"
+                          "\n"
+                          "uniform mat4 u_m4Model;\n"
+                          "uniform mat4 u_m4View;\n"
+                          "uniform mat4 u_m4Projection;\n"
+                          "\n"
+                          "uniform vec3 u_f3LightPos_Vs;\n"
+                          "\n"
+                          "out vec2 v_f2TextCor_Fs;\n"
+                          "out vec3 v_f3NormalDir_Vs;\n"
+                          "out vec3 v_f3CamDir_Vs;\n"
+                          "out vec3 v_f3LightDir_Vs;\n"
+                          "\n"
+                          "void main()\n"
+                          "{\n"
+                          "    gl_Position = u_m4Projection * u_m4View * u_m4Model * vec4(position,1);\n"
+                          "\n"
+                          "    vec3 vertexPos_Vs = ( u_m4View * u_m4Model * vec4(position,1)).xyz;\n"
+                          "    v_f3CamDir_Vs = vec3(0,0,0) - vertexPos_Vs;\n"
+                          "\n"
+                          "    v_f3LightDir_Vs = u_f3LightPos_Vs-vertexPos_Vs;\n"
+                          "\n"
+                          "    v_f3NormalDir_Vs = ( u_m4View * u_m4Model * vec4(normal,0)).xyz;\n"
+                          "\n"
+                          "    v_f2TextCor_Fs = uv0;\n"
+                          "}\n");
+
+        ::Ogre::GpuProgramParametersSharedPtr vertexParameters = vertex->getDefaultParameters();
+        vertexParameters->setNamedAutoConstant("u_m4Model", ::Ogre::GpuProgramParameters::ACT_WORLD_MATRIX);
+        vertexParameters->setNamedAutoConstant("u_m4View", ::Ogre::GpuProgramParameters::ACT_VIEW_MATRIX);
+        vertexParameters->setNamedAutoConstant("u_m4Projection", ::Ogre::GpuProgramParameters::ACT_PROJECTION_MATRIX);
+
+        vertexParameters->setNamedAutoConstant("u_f3LightPos_Vs", ::Ogre::GpuProgramParameters::ACT_LIGHT_POSITION_VIEW_SPACE, 0);
+
+        vertex->load();
+
+        pass0->setVertexProgram(vertex->getName());
+        pass1->setVertexProgram(vertex->getName());
+        pass2->setVertexProgram(vertex->getName());
+    }
+
+    {
+        Ogre::HighLevelGpuProgramPtr fragment = Ogre::HighLevelGpuProgramManager::getSingletonPtr()->createProgram(m_name + "_FS", m_group, "glsl", Ogre::GpuProgramType::GPT_FRAGMENT_PROGRAM);
+        fragment->setSource("#version 440 core\n"
+                            "\n"
+                            "in vec2 v_f2TextCor_Fs;\n"
+                            "in vec3 v_f3NormalDir_Vs;\n"
+                            "in vec3 v_f3CamDir_Vs;\n"
+                            "in vec3 v_f3LightDir_Vs;\n"
+                            "\n"
+                            "uniform sampler2D u_tAmbient;\n"
+                            "uniform sampler2D u_tDiffuse;\n"
+                            "uniform sampler2D u_tNormal;\n"
+                            "uniform sampler2D u_tSpecular;\n"
+                            "\n"
+                            "uniform vec3 u_f3AmbientCol;\n"
+                            "uniform vec3 u_f3DiffuseCol;\n"
+                            "uniform vec3 u_f3SpecularCol;\n"
+                            "uniform float u_fSpecularExponent;\n"
+                            "\n"
+                            "uniform vec3 u_f3LightAmbientCol;\n"
+                            "uniform vec3 u_f3LightDiffuseCol;\n"
+                            "uniform vec3 u_f3LightSpecularCol;\n"
+                            "\n"
+                            "layout(location = 0) out vec4 v_f4OutCol;\n"
+                            "\n"
+                            "void main()\n"
+                            "{\n"
+                            "    vec3 f3NormalDir_VsN = normalize(v_f3NormalDir_Vs);\n"
+                            "    vec3 f3LightDir_VsN = normalize(v_f3LightDir_Vs);\n"
+                            "    float cosTheta = clamp( dot(f3NormalDir_VsN, f3LightDir_VsN), 0, 1);\n"
+                            "    vec3 f3CamDir_VsN = normalize(v_f3CamDir_Vs);\n"
+                            "    vec3 f3CamReflectDir_VsN = reflect(-f3LightDir_VsN,f3NormalDir_VsN);\n"
+                            "    float cosAlpha = clamp(dot(f3CamDir_VsN, f3CamReflectDir_VsN), 0, 1);\n"
+                            "\n"
+                            "    // ambient\n"
+                            "    vec3 f3AmbientCol = texture(u_tAmbient, vec2(v_f2TextCor_Fs.x, 1 - v_f2TextCor_Fs.y)).rgb;\n"
+                            "    f3AmbientCol = f3AmbientCol * u_f3LightAmbientCol;\n"
+                            "\n"
+                            "    // diffuse\n"
+                            "    vec4 f4DiffuseCol = texture(u_tDiffuse, vec2(v_f2TextCor_Fs.x, 1 - v_f2TextCor_Fs.y));\n"
+                            "    vec3 f3DiffuseCol = f4DiffuseCol.rgb;\n"
+                            "    float fAlpha = f4DiffuseCol.a;\n"
+                            "    f3DiffuseCol = f3DiffuseCol * u_f3LightDiffuseCol * cosTheta;\n"
+                            "\n"
+                            "    // specular\n"
+                            "    vec3 f3SpecularCol = texture(u_tSpecular, vec2(v_f2TextCor_Fs.x, 1 - v_f2TextCor_Fs.y)).rgb;\n"
+                            "    f3SpecularCol = f3SpecularCol * u_f3LightSpecularCol * pow(cosAlpha, u_fSpecularExponent);\n"
+                            "\n"
+                            "    v_f4OutCol = vec4(u_f3AmbientCol * f3AmbientCol + u_f3DiffuseCol * f3DiffuseCol + u_f3SpecularCol * f3SpecularCol ,fAlpha);\n"
+                            "}\n"
+                            "\n");
+
+        ::Ogre::GpuProgramParametersSharedPtr fragmentParameters = fragment->getDefaultParameters();
+        fragmentParameters->setNamedAutoConstant("u_f3AmbientCol", ::Ogre::GpuProgramParameters::ACT_SURFACE_AMBIENT_COLOUR);
+        fragmentParameters->setNamedAutoConstant("u_f3DiffuseCol", ::Ogre::GpuProgramParameters::ACT_SURFACE_DIFFUSE_COLOUR);
+        fragmentParameters->setNamedAutoConstant("u_f3SpecularCol", ::Ogre::GpuProgramParameters::ACT_SURFACE_SPECULAR_COLOUR);
+        fragmentParameters->setNamedAutoConstant("u_fSpecularExponent", ::Ogre::GpuProgramParameters::ACT_SURFACE_SHININESS);
+
+        fragmentParameters->setNamedAutoConstant("u_f3LightAmbientCol", ::Ogre::GpuProgramParameters::ACT_AMBIENT_LIGHT_COLOUR, 0);
+        fragmentParameters->setNamedAutoConstant("u_f3LightDiffuseCol", ::Ogre::GpuProgramParameters::ACT_LIGHT_DIFFUSE_COLOUR, 0);
+        fragmentParameters->setNamedAutoConstant("u_f3LightSpecularCol", ::Ogre::GpuProgramParameters::ACT_LIGHT_SPECULAR_COLOUR, 0);
+
+        fragmentParameters->setNamedConstant("u_tAmbient", 0);
+        fragmentParameters->setNamedConstant("u_tDiffuse", 1);
+        fragmentParameters->setNamedConstant("u_tNormal", 2);
+        fragmentParameters->setNamedConstant("u_tSpecular",3);
+
+        fragment->load();
+
+        pass0->setFragmentProgram(fragment->getName());
+        pass1->setFragmentProgram(fragment->getName());
+        pass2->setFragmentProgram(fragment->getName());
+    }
+
+    {
+        ::Ogre::TextureUnitState* pTex1 = pass0->createTextureUnitState();
+        pTex1->setTextureName("rock_diffuse.jpg");
+        pTex1->setTextureFiltering(::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR);
+        ::Ogre::TextureUnitState* pTex2 = pass0->createTextureUnitState();
+        pTex2->setTextureName("rock_diffuse.jpg");
+        pTex2->setTextureFiltering(::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR);
+        ::Ogre::TextureUnitState* pTex3 = pass0->createTextureUnitState();
+        pTex3->setTextureName("rock_normal.jpg");
+        pTex3->setTextureFiltering(::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR);
+        ::Ogre::TextureUnitState* pTex4 = pass0->createTextureUnitState();
+        pTex4->setTextureName("rock_specular.png");
+        pTex4->setTextureFiltering(::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR);
+    }
+
+    {
+        ::Ogre::TextureUnitState* pTex1 = pass1->createTextureUnitState();
+        pTex1->setTextureName("rock_diffuse.jpg");
+        pTex1->setTextureFiltering(::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR);
+        ::Ogre::TextureUnitState* pTex2 = pass1->createTextureUnitState();
+        pTex2->setTextureName("rock_diffuse.jpg");
+        pTex2->setTextureFiltering(::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR);
+        ::Ogre::TextureUnitState* pTex3 = pass1->createTextureUnitState();
+        pTex3->setTextureName("rock_normal.jpg");
+        pTex3->setTextureFiltering(::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR);
+        ::Ogre::TextureUnitState* pTex4 = pass1->createTextureUnitState();
+        pTex4->setTextureName("rock_specular.png");
+        pTex4->setTextureFiltering(::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR);
+    }
+
+    {
+        ::Ogre::TextureUnitState* pTex1 = pass2->createTextureUnitState();
+        pTex1->setTextureName("rock_diffuse.jpg");
+        pTex1->setTextureFiltering(::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR);
+        ::Ogre::TextureUnitState* pTex2 = pass2->createTextureUnitState();
+        pTex2->setTextureName("rock_diffuse.jpg");
+        pTex2->setTextureFiltering(::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR);
+        ::Ogre::TextureUnitState* pTex3 = pass2->createTextureUnitState();
+        pTex3->setTextureName("rock_normal.jpg");
+        pTex3->setTextureFiltering(::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR);
+        ::Ogre::TextureUnitState* pTex4 = pass2->createTextureUnitState();
+        pTex4->setTextureName("rock_specular.png");
+        pTex4->setTextureFiltering(::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR, ::Ogre::FilterOptions::FO_LINEAR);
+    }
+}
+
+void Material::unload()
+{
+    std::cout << "[" << __FUNCTION__ << "] " << m_name << "..." << std::endl;
+    ::Ogre::MaterialManager::getSingleton().remove(m_name, m_group);
+    m_material.reset();
+}
