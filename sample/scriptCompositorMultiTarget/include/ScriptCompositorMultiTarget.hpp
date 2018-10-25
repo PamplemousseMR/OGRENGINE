fa3c1a68787236c55ea8f4b1c561fbd397c62386
pamplemousseMR <romain.manciaux@gmail.com>
2018-10-25 20:30:18 +0200

    feat(ScriptCompositorMultiTarget): add sample

---
+++ sample/scriptCompositorMultiTarget/include/ScriptCompositorMultiTarget.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "ResourceManager.hpp"
+#include "RenderSystemManager.hpp"
+
+#include <OGRE/Ogre.h>
+
+class ScriptCompositorMultiTarget : public Ogre::FrameListener
+{
+
+public:
+
+    ScriptCompositorMultiTarget();
+    virtual ~ScriptCompositorMultiTarget() override;
+
+    void start();
+	void stop();
+	void run();
+
+private:
+
+    RenderSystemManager m_renderSystemLoader;
+    ResourceManager m_resourceLoader;
+
+	::Ogre::LogManager* m_log;
+	::Ogre::Root* m_root;
+	::Ogre::SceneManager* m_sceneManager;
+	::Ogre::RenderWindow* m_renderWindow;
+
+	::Ogre::Viewport* m_viewport;
+	::Ogre::Camera* m_camera;
+
+    ::Ogre::Light* m_light;
+    ::Ogre::SceneNode* m_cube;
+
+    ::Ogre::SceneNode* m_cameraNode;
+
+	void loadScene();
+	void createObject();
+
+	void unloadScene();
+	void destroyObject();
+
+	// ================================= Events =================================
+
+	bool frameStarted(const Ogre::FrameEvent &evt) override;
+	bool frameEnded(const Ogre::FrameEvent &evt) override;
+	bool frameRenderingQueued(const Ogre::FrameEvent& evt) override;
+};
---
+++ sample/scriptCompositorMultiTarget/src/main.cpp
@@ -0,0 +1,23 @@
+#include "ScriptCompositorMultiTarget.hpp"
+
+#include <iostream>
+
+int main()
+{
+    try {
+        ScriptCompositorMultiTarget application;
+        application.start();
+        application.run();
+        application.stop();
+    }
+    catch (Ogre::Exception &e) {
+        std::cout << "[" << __FUNCTION__ << "] " << e.getFullDescription().c_str() << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cout << "[" << __FUNCTION__ << "] " << e.what() << std::endl;
+    }
+
+    std::cout << "[" <<__FUNCTION__ << "] " << "finished" << std::endl;
+
+    return 0;
+}
